bd04f782dc126b133651008f9f2490155027326a
Brandi Apetsi <brandi@Brandis-MacBook.local>
2019-05-28 18:41:20 -0400

    added get by id to posts-router

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -17,6 +17,16 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id)
 
+        post ? res.status(200).json(post) :  res.status(404).json({error: 'The post with the spicified ID does not exist.'})
+    } catch (error) {
+        res.status(500).json({
+            error: 'The post information could notbe retrieved.'
+        })
+    }
+})
 
 module.exports = router
